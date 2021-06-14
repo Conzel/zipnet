46b94ae58182cf08b854c22746156d037f47a45f
Alex <38732545+Conzel@users.noreply.github.com>
2021-06-14 16:01:46 +0200

    ADD: Dummy coders

---
+++ coders/src/lib.rs
@@ -0,0 +1,27 @@
+pub mod coding_errors;
+pub mod dummy_coders;
+
+pub type EncodedData = Vec<u32>;
+pub type CodingResult<T> = std::result::Result<T, coding_errors::CodingError>;
+
+/// The trait of an encoder. An encoder can take data of type T and returns a vector
+/// of 32-bit integers, that contain the compress data.
+pub trait Encoder<T> {
+    /// Encodes the given data into a 32-bit vector.
+    fn encode(&mut self, data: &T) -> EncodedData;
+}
+
+/// The trait of a decoder. An decoder can take data of type T and returns a vector
+/// of 32-bit integers, that contain the compress data.
+pub trait Decoder<T> {
+    /// Decodes the given data.
+    fn decode(&mut self, encoded_data: &EncodedData) -> CodingResult<T>;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
