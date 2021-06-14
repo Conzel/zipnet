76a58333c6d2967c84ad67584c8f7cd6b9128d05
Alex <38732545+Conzel@users.noreply.github.com>
2021-06-14 20:25:07 +0200

    ADD: Simple example on how to read an image from Javascript

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,5 +1,6 @@
 extern crate js_sys;
 extern crate web_sys;
+use image::{load_from_memory_with_format, GenericImageView, ImageFormat};
 
 mod utils;
 
@@ -12,13 +13,22 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, wasm-template-rust!");
+}
+
+#[wasm_bindgen]
+pub fn process_png_image(buffer: Vec<u8>) -> u32 {
+    // Enables us to get better error messages in the browser
+    console_error_panic_hook::set_once();
+
+    let result = load_from_memory_with_format(&buffer, ImageFormat::Png).unwrap();
+    result.width()
 }
 
 #[cfg(test)]
--- wasm/www/index.js
+++ wasm/www/index.js
@@ -1,3 +1,27 @@
 import * as wasm from "wasm-zipnet";
 
-wasm.greet();
+// wasm.greet();
+
+// Displays a file selected by the user in a preview.
+function previewFile() {
+    var preview = document.querySelector('img');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+        // preview.src = reader.result;
+
+        var data = reader.result;
+        var len = wasm.process_png_image(new Uint8Array(data));
+        alert(len);
+    }
+
+    if (file) {
+        reader.readAsArrayBuffer(file)
+
+        // reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+}
+document.querySelector('input').addEventListener("change", previewFile);
