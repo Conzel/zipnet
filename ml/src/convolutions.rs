80f208ed7912d8d9d52454eb038685a97fa08e77
Alex <38732545+Conzel@users.noreply.github.com>
2021-06-15 19:25:14 +0200

    CHANGE: Made conv2d a bit more ergonomic to use
    Now accepting any array that can be turned into an array view

--- ml/src/convolutions.rs
+++ ml/src/convolutions.rs
@@ -6,6 +6,7 @@ use ndarray::*;
 /// input channels x output channels x kernel width x kernel height
 /// (to comply with the order in which pytorch weights are saved).
 pub struct ConvolutionLayer {
+    /// Weight matrix of the kernel
     kernel: Array4<WeightPrecision>,
     kernel_width: usize,
     kernel_height: usize,
@@ -16,16 +17,20 @@ pub struct ConvolutionLayer {
 }
 
 impl ConvolutionLayer {
-    pub fn new(kernel: Array4<WeightPrecision>, stride: usize, padding: usize) -> ConvolutionLayer {
-        let num_input_channels = kernel.len_of(Axis(0)) as u16;
-        let num_output_channels = kernel.len_of(Axis(1)) as u16;
-        let kernel_width = kernel.len_of(Axis(2));
-        let kernel_height = kernel.len_of(Axis(3));
+    pub fn new(
+        weights: Array4<WeightPrecision>,
+        stride: usize,
+        padding: usize,
+    ) -> ConvolutionLayer {
+        let num_input_channels = weights.len_of(Axis(0)) as u16;
+        let num_output_channels = weights.len_of(Axis(1)) as u16;
+        let kernel_width = weights.len_of(Axis(2));
+        let kernel_height = weights.len_of(Axis(3));
 
         debug_assert!(stride > 0, "Stride of 0 passed");
 
         ConvolutionLayer {
-            kernel,
+            kernel: weights,
             kernel_width,
             kernel_height,
             stride,
@@ -36,24 +41,29 @@ impl ConvolutionLayer {
     }
 
     /// Performs a convolution on the given image data using this layers parameters.
-    pub fn conv(&self, image: &Array3<ImagePrecision>) {
+    pub fn convolve(&self, image: &Array3<ImagePrecision>) -> Array3<ImagePrecision> {
         todo!();
     }
 
     /// Naive implementation of 2d convolution
     /// TODO: We might want to get something more efficient going, like described here:
     /// https://wiseodd.github.io/techblog/2016/07/16/convnet-conv-layer/
-    fn conv_2d(
-        &self,
-        kernel_weights: &ArrayView2<WeightPrecision>,
-        im2d: &ArrayView2<ImagePrecision>,
-    ) -> Array2<ImagePrecision> {
+    fn conv_2d<'a, T, V>(&self, kernel_weights: T, im2d: V) -> Array2<ImagePrecision>
+    where
+        // This trait bound ensures that kernel and im2d can be passed as owned array or view.
+        // AsArray just ensures that im2d can be converted to an array view via ".into()".
+        // Read more here: https://docs.rs/ndarray/0.12.1/ndarray/trait.AsArray.html
+        V: AsArray<'a, ImagePrecision, Ix2>,
+        T: AsArray<'a, ImagePrecision, Ix2>,
+    {
         if self.padding > 0 {
             unimplemented!("Padding bigger than 0 is not supported yet.")
         }
+        let im2d_arr: ArrayView2<f32> = im2d.into();
+        let kernel_weights_arr: ArrayView2<f32> = kernel_weights.into();
 
-        let im_width = im2d.len_of(Axis(0));
-        let im_height = im2d.len_of(Axis(1));
+        let im_width = im2d_arr.len_of(Axis(0));
+        let im_height = im2d_arr.len_of(Axis(1));
 
         let new_im_width = (im_width - self.kernel_width) / self.stride + 1;
         let new_im_height = (im_height - self.kernel_height) / self.stride + 1;
@@ -64,12 +74,12 @@ impl ConvolutionLayer {
             let i_with_stride = i * self.stride;
             for j in 0..new_im_height {
                 let j_with_stride = j * self.stride;
-                let imslice = im2d.slice(s![
+                let imslice = im2d_arr.slice(s![
                     i_with_stride..(i_with_stride + self.kernel_width),
                     j_with_stride..j_with_stride + (self.kernel_height)
                 ]);
 
-                let conv_entry = (&imslice * kernel_weights).sum();
+                let conv_entry = (&imslice * &kernel_weights_arr).sum();
                 ret[[i, j]] = conv_entry;
             }
         }
@@ -97,14 +107,12 @@ mod tests {
 
     #[test]
     fn test_naive_2d_conv_with_stride() {
-        let test_img = array![[0., 1., 0.], [0., 0., 0.], [-1., 0., 0.]];
+        let test_img: Array2<ImagePrecision> = array![[0., 1., 0.], [0., 0., 0.], [-1., 0., 0.]];
         let kernel = Array::from_shape_vec((1, 1, 1, 1), vec![1.]).unwrap();
         let conv_layer = ConvolutionLayer::new(kernel, 2, 0);
 
-        let convolved_image = conv_layer.conv_2d(
-            &(conv_layer.kernel.slice(s![0, 0, .., ..])),
-            &test_img.view(),
-        );
+        let convolved_image =
+            conv_layer.conv_2d(&(conv_layer.kernel.slice(s![0, 0, .., ..])), &test_img);
 
         assert_eq!(convolved_image, array![[0., 0.], [-1., 0.]]);
     }
