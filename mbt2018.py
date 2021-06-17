79a030ed2fdf6e2abe3ec0e56a032542bc01d022
JohanvandenHeuvel <johanheuvel5@gmail.com>
2021-06-17 10:45:33 +0200

    feature/decompression-works

--- mbt2018.py
+++ mbt2018.py
@@ -26,6 +26,7 @@ import argparse
 import glob
 import sys
 import os
+import time
 
 from absl import app
 from absl.flags import argparse_flags
@@ -303,6 +304,7 @@ def main(args):
     if args.command == "train":
         train(args, build_train_graph=build_train_graph)
     elif args.command == "compress":
+        print(args)
         if not args.output_file:
             args.output_file = args.input_file + ".tfci"
         compress(args)
