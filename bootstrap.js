ae3032ac35746be41993e9fdc1af156834aca601
JohanvandenHeuvel <JohanvandenHeuvel@users.noreply.github.com>
2021-07-09 13:42:08 +0000

    Deploy 594d536416bbd6f662c6cb75a025d78e3726c1fd

--- bootstrap.js
+++ bootstrap.js
@@ -171,7 +171,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_bg.wasm":"99790c2af864c7bd3276"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_bg.wasm":"421f785da38776427bc5"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
