abbb0b25892952dc79bd316fa1f8192d78018e91
Alex <38732545+Conzel@users.noreply.github.com>
2021-08-11 14:00:10 +0200

    CHANGE: Updated webpack config to work with emscripten
    The default webpack options don't play with wasm32-emscripten.
    Refer to the open issue in: https://github.com/webpack/webpack/issues/7352

--- wasm/www/webpack.config.js
+++ wasm/www/webpack.config.js
@@ -1,4 +1,5 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
 const path = require('path');
 
 module.exports = {
@@ -11,4 +12,22 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin(['index.html'])
   ],
+  // refer to https://github.com/webpack/webpack/issues/7352
+  // Hack to make it work with emscripten-wasm
+  node: { 
+    "fs": "empty" // ← !!
+  },
+  module: {
+    rules: [
+      /* ... */
+      {
+        test: /fibonacci\.wasm$/,
+        type: "javascript/auto", // ← !!
+        loader: "file-loader",
+        options: {
+          publicPath: "dist/"
+        }
+      }
+    ]
+  },
 };
