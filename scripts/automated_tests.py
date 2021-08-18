7bbc55c509b67564765c14edfefd34f727706fe8
Alex <38732545+Conzel@users.noreply.github.com>
2021-08-18 17:36:39 +0200

    ADD: Basic scaffolding for auto-generated tests

---
+++ scripts/automated_tests.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import jinja2
+import numpy as np
+
+class TestObject:
+"""The structs that are used in the Jinja templates."""
+    def __init__(self, layer_name, input_arr, output_arr, kernel, stride=1, padding=0):
+        assert!(layer_name == "ConvolutionLayer" || layer_name == "TransposedConvolutionLayer", "Layer name unknown")
+        self.input_arr = input_arr
+        self.output_arr = output_arr
+        self.kernel = kernel
+        self.stride = stride
+        self.padding = padding
+
+def numpy_array_to_rust(x):
+    """
+        Outputs a numpy array as a Rust ndarray.
+    """
+    array_repr = f"{x}".replace("\n", ",\n\t\t")
+    return f"array!{array_repr}"
+
+np.set_printoptions(suppress=True)
+
+loader = jinja2.FileSystemLoader("./templates")
+env = jinja2.Environment(loader=loader)
+
+template = env.get_template("test_py_impl_random_arrays_template.rs")
+
+num_arrs = 10
+
+random_arrays = [numpy_array_to_rust(np.random.rand(5,5,5,5)) 
+        for x in range(num_arrs)]
+
+
