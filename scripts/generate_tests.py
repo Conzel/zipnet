c73a2e54165286e9b532b5e863f1e777ffd26dfc
Alex <38732545+Conzel@users.noreply.github.com>
2021-08-20 20:02:00 +0200

    ADD: Generate tests now works with the new padding

--- scripts/generate_tests.py
+++ scripts/generate_tests.py
@@ -24,20 +24,27 @@ class RandomArrayTest:
         self.random_test_objects = random_test_objects
 
 class RandomArrayTestObject:
-    def __init__(self, input_arr, kernel, output_arr, stride=1, padding=0):
+    def __init__(self, input_arr, kernel, output_arr, padding, stride=1):
         """Struct that represents one test case for the random array tests.
         input_arr: ndarray, 3-Dimensional floating point numpy array
         output_arr: ndarray, 3-Dimensional floating point numpy array
         kernel: ndarray, 3-Dimensional floating point numpy array, weights of
         the convolutional layer
         stride: int
-        padding: int
+        padding: str, valid or same. Valid padding just applies the kernel directly,
+        same padding ensures that inputsize = outputsize
         """
+        if padding == "VALID":
+            self.padding = "Padding::Valid"
+        elif padding == "SAME":
+            self.padding = "Padding::Same"
+        else:
+            raise ValueError(f"Illegal padding value {padding}")
+
         self.input_arr = numpy_array_to_rust(input_arr)
         self.output_arr = numpy_array_to_rust(output_arr)
         self.kernel = numpy_array_to_rust(kernel, shape_vec=True)
         self.stride = stride
-        self.padding = padding
 
 def numpy_array_to_rust(x, shape_vec=False):
     """
@@ -66,8 +73,7 @@ def conv2d_random_array_test(num_arrays_per_case=3):
     per (img_shape, kernel_shape) combination"""
     img_shapes = [(5,5,1), (10,15,1), (15,10,1), (6,6,3), (10,15,3), (15,10,3)]
     kernel_shapes = [(3,3,1,3), (5,5,1,2), (3,3,3,2), (5,5,3,2)]
-    
-    # padding and strides are 1 for now
+    padding = "VALID"
 
     objects = []
     for im_shape, ker_shape in list(itertools.product(img_shapes, kernel_shapes)):
@@ -79,7 +85,7 @@ def conv2d_random_array_test(num_arrays_per_case=3):
             # axis 0 is batch dimension, which we need to remove and add back in
             im_tf = tf.constant(np.expand_dims(im, axis=0), dtype=tf.float64)
             ker_tf = tf.constant(ker, dtype=tf.float64)
-            out_tf = tf.nn.conv2d(im_tf, ker_tf, strides=[1,1,1,1], padding="VALID")
+            out_tf = tf.nn.conv2d(im_tf, ker_tf, strides=[1,1,1,1], padding=padding)
             out = np.squeeze(out_tf.numpy(), axis=0)
 
             # reordering the images and weights
@@ -99,7 +105,7 @@ def conv2d_random_array_test(num_arrays_per_case=3):
             ker = np.moveaxis(ker, [0,1,2,3], [3,2,0,1])
             out = np.moveaxis(out, [0,1,2], [1,2,0])
 
-            test_obj = RandomArrayTestObject(im, ker, out)
+            test_obj = RandomArrayTestObject(im, ker, out, padding)
             objects.append(test_obj)
    
     return RandomArrayTest("conv2d", "ConvolutionLayer", objects)
