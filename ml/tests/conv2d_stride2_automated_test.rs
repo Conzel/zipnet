216d712a5182d03dde5a51f765880e277d285986
Alex <38732545+Conzel@users.noreply.github.com>
2021-09-14 15:55:12 +0200

    CHANGE: Improved error messages for tests
    Now aborts early in case of shape mismatch and reports exact mismatch.

--- ml/tests/conv2d_stride2_automated_test.rs
+++ ml/tests/conv2d_stride2_automated_test.rs
@@ -8,8 +8,15 @@ use ml::transposed_convolutions::*;
 #[allow(unused_imports)]
 use ndarray::{array, Array, Array3, Array4, Dimension};
 
-fn arr_allclose<D: Dimension>(arr1: &Array<f32, D>, arr2: &Array<f32, D>) -> bool {
-    (arr1 - arr2).map(|x| (*x as f32).abs()).sum() < 1e-3
+fn arr_allclose<D: Dimension>(input: &Array<f32, D>, target: &Array<f32, D>) -> bool {
+    assert_eq!(
+        input.shape(),
+        target.shape(),
+        "\ninput had shape {:?}, but target had shape {:?}",
+        input.shape(),
+        target.shape()
+    );
+    (input - target).map(|x| (*x as f32).abs()).sum() < 1e-3
 }
 
 #[test]
