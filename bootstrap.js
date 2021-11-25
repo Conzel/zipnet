7a4496e8a381283fde03a8f90c1f2447345c484a
Conzel <Conzel@users.noreply.github.com>
2021-11-25 15:07:47 +0000

    Deploy 7fcb4b06a0c3edc7773d96241d9e739be97bb938

--- bootstrap.js
+++ bootstrap.js
@@ -171,7 +171,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_bg.wasm":"45ded8bf09afdf147119"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_bg.wasm":"d6c2b0fc8a21eee489ae"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
